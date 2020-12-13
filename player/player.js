8a66f974ae9a70c48f4eb890f9887f1d9621486d
Spycro <lucaszim16@gmail.com>
2020-12-13 19:04:17 +0100

    refactoring into player.js file
    TODO: 
    search.js
    a uniform song struct (function to make it?)

---
+++ player/player.js
@@ -0,0 +1,65 @@
+const ytdl = require('ytdl-core');
+
+function test(abc) {
+	console.log(abc);
+}
+
+const playMusic = async (song, message) => {
+	const serverQueue = message.client.queue.get(message.guild.id);
+
+	const { channel } = message.member.voice;
+	if (serverQueue) {
+		serverQueue.songs.push(song);
+		console.log(serverQueue.songs);
+		return message.channel.send(`✅ **${song.title}** has been added to the queue!`);
+	}
+
+	const queueConstruct = {
+		textChannel: message.channel,
+		voiceChannel: channel,
+		connection: null,
+		songs: [],
+		volume: 2,
+		playing: true,
+		looping: false,
+	};
+	message.client.queue.set(message.guild.id, queueConstruct);
+	queueConstruct.songs.push(song);
+
+	const play = async (song) => {
+		const queue = message.client.queue.get(message.guild.id);
+		if (!song) {
+			queue.voiceChannel.leave();
+			message.client.queue.delete(message.guild.id);
+			return;
+		}
+
+		const dispatcher = queue.connection.play(ytdl(song.url))
+			.on('finish', () => {
+				if(!queue.looping) {
+					queue.songs.shift();
+					play(queue.songs[0]);
+				}
+				else{
+					play(queue.songs[0]);
+				}
+			})
+			.on('error', error => console.error(error));
+		dispatcher.setVolumeLogarithmic(queue.volume / 5);
+		queue.textChannel.send(`🎶 Start playing: **${song.title}**`);
+	};
+
+	try {
+		const connection = await channel.join();
+		queueConstruct.connection = connection;
+		play(queueConstruct.songs[0]);
+	}
+	catch (error) {
+		console.error(`I could not join the voice channel: ${error}`);
+		message.client.queue.delete(message.guild.id);
+		await channel.leave();
+		return message.channel.send(`I could not join the voice channel: ${error}`);
+	}
+};
+
+module.exports = { name: 'test', test, playMusic };
